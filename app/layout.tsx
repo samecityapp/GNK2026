049b9e6688a25a1565fdbf08c60ccd9eb38051d6
samecityapp <erdempakyollu@gmail.com>
2025-11-21 14:40:04 +0300

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode; }) {
   return (
     <html lang="tr" className={inter.variable}>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+      </head>
       <body className="font-sans">
         <Providers>
           <Header />
