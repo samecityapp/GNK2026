605b14674cbe0273dc5aaa8e86ee67f1af55e018
samecityapp <erdempakyollu@gmail.com>
2025-11-24 18:23:36 +0300

    Updated page.tsx

--- app/otel/[id]/page.tsx
+++ app/otel/[id]/page.tsx
@@ -193,14 +193,14 @@ export default async function HotelDetailPage({ params }: Props) {
           </div>
 
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mb-6">
-            <div className="lg:col-span-2 lg:row-start-1">
+            <div className="lg:col-span-3">
               <HotelFeatures tags={hotelTagsWithIcons} />
             </div>
-            <div className="lg:col-span-1 lg:row-start-1">
-              <LocationCard latitude={hotel.latitude} longitude={hotel.longitude} address={hotel.location} />
-            </div>
-            <div className="lg:col-span-3 lg:row-start-2">
+            <div className="lg:col-span-2">
               <HotelDescription about={hotel.about || ''} />
+            </div>
+            <div className="lg:col-span-1">
+              <LocationCard latitude={hotel.latitude} longitude={hotel.longitude} address={hotel.location} />
             </div>
           </div>
 
